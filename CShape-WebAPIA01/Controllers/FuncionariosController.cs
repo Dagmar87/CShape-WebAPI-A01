c6c80414a9b2e766db7339e14398af8c0e50580f
Dagmar87 <jdfssobrinho@hotmail.com>
2022-10-10 11:47:19 -0300

    criação do projeto e etapa REST - GETALL

---
+++ CShape-WebAPIA01/Controllers/FuncionariosController.cs
@@ -0,0 +1,82 @@
+﻿using CShape_WebAPIA01.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CShape_WebAPIA01.Controllers
+{
+    [Route("api/funcionarios")]
+    [ApiController]
+    public class FuncionariosController : ControllerBase
+    {
+        private readonly Funcionario[] Funcionarios = new Funcionario[]
+        {
+            new Funcionario
+            {
+                Codigo = 1,
+                Nome = "Renato",
+                Sobrenome = "Silva",
+                Setor = "TI",
+                Cidade = "Recife"
+            },
+            new Funcionario
+            {
+                Codigo = 2,
+                Nome = "Catarina",
+                Sobrenome = "Rodrigues",
+                Setor = "RH",
+                Cidade = "Olinda"
+            },
+            new Funcionario
+            {
+                Codigo = 3,
+                Nome = "Julio",
+                Sobrenome = "Coelho",
+                Setor = "ADM",
+                Cidade = "Paulista"
+            },
+            new Funcionario
+            {
+                Codigo = 4,
+                Nome = "Fabiana",
+                Sobrenome = "Moura",
+                Setor = "Patrimônio",
+                Cidade = "Jaboatão dos Guararapes"
+            },
+        };
+
+        // GET api/funcionario
+        [HttpGet]
+        public Funcionario[] Get()
+        {
+            return Funcionarios;
+        }
+
+        // GET api/funcionario/5
+        [HttpGet("{codigo}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/funcionario
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+
+        }
+
+        // PUT api/funcionario/5
+        [HttpPut("{codigo}")]
+        public void Put(int id, [FromBody]string value)
+        {
+
+        }
+
+        // DELETE api/funcionario/5
+        [HttpDelete("{codigo}")]
+        public void Delete(int id)
+        {
+
+        }     
+    }
+}
---
+++ CShape-WebAPIA01/Models/Funcionario.cs
@@ -0,0 +1,11 @@
+﻿namespace CShape_WebAPIA01.Models
+{
+    public class Funcionario
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Setor { get; set; }
+        public string Cidade { get; set; }
+    }
+}
